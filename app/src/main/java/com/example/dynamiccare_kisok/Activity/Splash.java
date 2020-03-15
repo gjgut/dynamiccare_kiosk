da56b2b5149f1fedb9e37befeb51e9988f93e173
gjgut <gjgustjd70@gmail.com>
2020-03-15 13:55:56 +0900

    Splash 예외처리

--- app/src/main/java/com/example/dynamiccare_kisok/Activity/Splash.java
+++ app/src/main/java/com/example/dynamiccare_kisok/Activity/Splash.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import com.example.dynamiccare_kisok.Common.DynamicCare;
 import com.example.dynamiccare_kisok.R;
@@ -13,16 +14,21 @@ public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        DynamicCare care = (DynamicCare)getApplicationContext();
-        care.initSounds();
-        Handler hd = new Handler();
-        hd.postDelayed(new splashhandler(), 1000); // 1초 후에 hd handler 실행  3000ms = 3초
+        try {
+            super.onCreate(savedInstanceState);
+            DynamicCare care = (DynamicCare) getApplicationContext();
+            care.initSounds();
+            Handler hd = new Handler();
+            hd.postDelayed(new splashhandler(), 1000); // 1초 후에 hd handler 실행  3000ms = 3초
+        } catch (Exception e) {
+            Log.i("Error", e.toString());
+        }
+
 
     }
 
-    private class splashhandler implements Runnable{
-        public void run(){
+    private class splashhandler implements Runnable {
+        public void run() {
             startActivity(new Intent(getApplication(), Login.class)); //로딩이 끝난 후, ChoiceFunction 이동
             Splash.this.finish(); // 로딩페이지 Activity stack에서 제거
         }
